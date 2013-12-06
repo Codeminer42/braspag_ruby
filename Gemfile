ab7f90a951095102d18762f7276d0a08088b0e4c
Mateus Gomes <mateusg.18@gmail.com>
2013-12-06 19:12:37 -0300

    Add simplecov for test coverage checking

--- Gemfile
+++ Gemfile
@@ -2,3 +2,7 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in braspag.gemspec
 gemspec
+
+group :test do
+  gem 'simplecov', require: false
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,9 @@
+if ENV['COVERAGE'] == '1'
+  require 'simplecov'
+
+  SimpleCov.start { add_filter 'spec' }
+end
+
 $:.unshift File.dirname(__FILE__) + "../lib"
 
 require "rubygems"
